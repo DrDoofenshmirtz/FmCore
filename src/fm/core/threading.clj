667a8404bcf6962366de30033e15d78c90e3f4c0
Frank Mosebach <frank@mausi.(none)>
2011-05-20 22:13:35 +0200

    Added doc strings.

--- src/fm/core/threading.clj
+++ src/fm/core/threading.clj
@@ -6,7 +6,10 @@
   (:use [fm.core.seq :only (split-with-tags)]))
 
 (defnk call-async
-  "..."
+  "Runs the given job function in a new thread. Returns the created thread.
+  Supported options are: :thread-name name prefix (defaults to \"do-async\",
+  a dash and the created thread's id will be appended), :priority thread
+  priority (defaults to Thread/NORM_PRIORITY)."
   [job :thread-name nil :priority nil]
   (let [thread (Thread. job)]
     (doto thread
@@ -15,7 +18,10 @@
       (.start))))
 
 (defmacro do-async
-  "..."
+  "Wraps the given forms in a job function that will be executed in a new
+  thread. Supported options are: :thread-name name prefix (defaults to
+  \"do-async\", a dash and the created thread's id will be appended),
+  :priority thread priority (defaults to Thread/NORM_PRIORITY)."
   [& forms-and-options]
   (let [[forms options] (split-with-tags 
                           forms-and-options
@@ -23,7 +29,8 @@
     `(call-async (fn [] ~@forms) ~@options)))
 
 (defmacro with-lock
-  "..."
+  "Executes the body forms after acquiring the lock. Ensures that the lock
+  will be released afterwards."
   [lock & body]
   `(do
      (.lock ~lock)
