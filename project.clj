e3ae3ac50f020be326fd2365cfb0d81c36e6e465
Frank Mosebach <frank@mausi.(none)>
2012-01-07 19:06:39 +0100

    Added projetc file for leiningen.

---
+++ project.clj
@@ -0,0 +1,16 @@
+(def local-repo-path (-> (java.io.File. "/home/frank/clojure/mvn-repo") .toURI str))
+
+(defproject fm/core "1.0.0-SNAPSHOT"  
+  :description "FmCore: Basic Clojure Utilities."  
+  :dependencies [[fm.clojure/clojure "1.2.0"]
+                 [fm.clojure/clojure-contrib "1.2.0"]]  
+  :disable-deps-clean true    
+  :repositories {"fm-local" ~local-repo-path}
+  :jar-name "fm-core.jar"           
+  :omit-source false
+  :jar-exclusions [#"(?:^|/).svn/" 
+                   #"(?:^|/).git/" 
+                   #"(?:^|/)project.clj"
+                   #"(?:^|/)scratch.clj"
+                   #"(?:^|/)canvas.clj"])
+
