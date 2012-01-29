7840726a9bea2f0aa506f710d6be493a91e197ed
Frank Mosebach <frank@mausi.(none)>
2012-01-29 13:36:48 +0100

    Check for matching option name when reading the next option value from the argument list.

--- src/fm/core/args.clj
+++ src/fm/core/args.clj
@@ -2,30 +2,46 @@
   ^{:doc "Utilities for processing function arguments."
     :author "Frank Mosebach"}
   fm.core.args
-  (:use
-    [clojure.contrib.except :only (throw-if)]
-    [clojure.contrib.str-utils :only (chop)]))
+  (:use [clojure.contrib.str-utils :only (chop)]))
 
-(defn flag-reader [default-value]
+(defn- throw-unexpected-option-name [expected actual]
+  (throw (IllegalArgumentException. (str
+                                      "Unexpected option name (expected: '"
+                                      expected "', actual: '" actual "')!"))))
+
+(defn- throw-missing-option-value [option-name]
+  (throw (IllegalStateException. (str
+                                   "Missing value for option '"
+                                   option-name "'!"))))
+
+(defn- throw-illegal-option-name [option-name]
+  (throw (IllegalArgumentException. (str
+                                      "Illegal option name: '"
+                                      option-name "'!"))))
+
+(defn flag-reader [option-name default-value]
   (fn
     ([] default-value)
-    ([[_ & args-tail]] [true args-tail])))
+    ([[next-name & args-tail]]
+      (if (= option-name next-name)
+        [true args-tail]
+        (throw-unexpected-option-name option-name next-name)))))
 
-(defn value-reader [default-value]
+(defn value-reader [option-name default-value]
   (fn
     ([] default-value)
     ([args]
-      (let [option-name-and-value (take 2 args)]
-        (throw-if
-          (< (count option-name-and-value) 2)
-          (IllegalStateException. (str
-                                    "Missing value for option '"
-                                    (first option-name-and-value)
-                                    "'!")))
-        [(second option-name-and-value) (drop 2 args)]))))
+      (let [[next-name next-value :as name-and-value] (take 2 args)]
+        (cond
+          (not= option-name next-name)
+            (throw-unexpected-option-name option-name next-name)
+          (< (count name-and-value) 2)
+            (throw-missing-option-value next-name)
+          :else
+            [next-value (drop 2 args)])))))
 
-(defn- option-reader [default-value flag-option?]
-  ((if flag-option? flag-reader value-reader) default-value))
+(defn- option-reader [option-name default-value flag-option?]
+  ((if flag-option? flag-reader value-reader) option-name default-value))
 
 (defn- reader-map [option-specs make-reader]
   (reduce
@@ -35,13 +51,10 @@
             flag-option? (.endsWith option-name "?")
             option-name (if flag-option? (chop option-name) option-name)
             option-name (keyword option-name)
-            option-reader (make-reader default-value flag-option?)]
+            option-reader (make-reader option-name default-value flag-option?)]
         (assoc reader-map option-name option-reader)))
     {}
     (partition 2 option-specs)))
-
-(defn- throw-illegal-option-name [option-name]
-  (throw (IllegalArgumentException. (str "Illegal option name: '" option-name "'!"))))
 
 (defn- read-options [args reader-map options]
   (if (seq args)
@@ -72,10 +85,10 @@
     (let [option-specs (cons option-name (cons default-value option-specs))]
       (make-options-builder (reader-map option-specs option-reader)))))
 
-(defn- gen-option-reader [default-value flag-option?]
+(defn- gen-option-reader [option-name default-value flag-option?]
   (if flag-option?
-   `(flag-reader ~default-value)
-   `(value-reader ~default-value)))
+   `(flag-reader ~option-name ~default-value)
+   `(value-reader ~option-name ~default-value)))
 
 (defn- gen-reader-map [option-specs]
   (reader-map option-specs gen-option-reader))
