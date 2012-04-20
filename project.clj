2dfec7af9e0a9ce358178004e04201c687426c7b
Frank <frank@perry.(none)>
2012-04-20 20:06:01 +0200

    Moved all unused or prototype sources to FmIncubator.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def local-repo-path (-> (java.io.File. "/home/frank/clojure/mvn-repo") .toURI str))
+(def local-repo-path (-> (java.io.File. "/home/frank/local-mvn-repo") .toURI str))
 
 (defproject fm/core "1.0.2"
   :description "FmCore: Basic Clojure Utilities."  
@@ -10,7 +10,5 @@
   :omit-source false
   :jar-exclusions [#"(?:^|/).svn/" 
                    #"(?:^|/).git/" 
-                   #"(?:^|/)project.clj"
-                   #"(?:^|/)scratch.clj"
-                   #"(?:^|/)canvas.clj"])
+                   #"(?:^|/)project.clj"])
 
